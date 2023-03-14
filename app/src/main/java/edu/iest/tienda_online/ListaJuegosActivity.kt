cd0aa9b5510253cb3c11cc233086d3d86e3793ce
GerardoValencia <gerardo.valencia@iest.edu.mx>
2023-03-13 19:08:39 -0600

    13/03/23-IEST

--- app/src/main/java/edu/iest/tienda_online/ListaJuegosActivity.kt
+++ app/src/main/java/edu/iest/tienda_online/ListaJuegosActivity.kt
@@ -1,4 +1,24 @@
 package edu.iest.tienda_online
 
-class ListaJuegosActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.iest.tienda_online.adapter.VideojuegosAdapter
+
+class ListaJuegosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_juegos)
+        val juegos = FakerVideojuego().getVideogames()
+        val recycler = findViewById<RecyclerView>(R.id.recyclerJuegos)
+
+        val CANTIDAD_COLUMNAS = 2
+        val administradorDeLayouts = LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL, false)
+        //LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        //GridLayoutManager(this, CANTIDAD_COLUMNAS)
+        recycler.layoutManager = administradorDeLayouts
+        recycler.adapter = VideojuegosAdapter(juegos, this)
+    }
 }
--- app/src/main/java/edu/iest/tienda_online/adapter/VideojuegosAdapter.kt
+++ app/src/main/java/edu/iest/tienda_online/adapter/VideojuegosAdapter.kt
@@ -1,5 +1,38 @@
 package edu.iest.tienda_online.adapter
 
-class VideojuegosAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import edu.iest.tienda_online.R
+import edu.iest.tienda_online.models.Videojuego
 
+class VideojuegosAdapter (videojuegos: ArrayList<Videojuego>, context: Context) :
+    RecyclerView.Adapter<VideojuegosAdapter.ContenedorDeVista>(){
+
+    var inner_videojuegos: ArrayList<Videojuego> = videojuegos
+    var inner_context: Context = context
+
+    inner class ContenedorDeVista(view: View) : RecyclerView.ViewHolder(view){
+        init {
+            // Define click listener for the ViewHolder's View.
+            tvNombre = view.findViewById(R.id.tvNombreJuego)
+            tvPrecio = view.findViewById(R.id.tvPrecio)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContenedorDeVista {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.videojuegos_layout,
+            viewGroup, false)
+
+        return ContenedorDeVista(view)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContenedorDeVista, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
